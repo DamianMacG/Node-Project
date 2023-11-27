702b00087a5cf09ba9d0d1b79c642099a363c51d
damiang <damiangildea12@gmail.com>
2023-11-27 13:40:25 +0000

    Creating a server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
+const fs = require("fs");
+const http = require("http");
 
-fs.readFile("./starter/txt/start.txt", "utf-8", (err, data) => {
-  fs.writeFile(`./starter/txt/output.txt`, `${data}`, "utf-8", err => {
-    console.log("DONE!!!!!!!!!!!");
-  });
-  
+// fs.readFile("./starter/txt/start.txt", "utf-8", (err, data) => {
+//   fs.writeFile(`./starter/txt/output.txt`, `${data}`, "utf-8", err => {
+//     console.log("DONE!!!!!!!!!!!");
+//   });
+// });
+
+const server = http.createServer((req, res) => {
+  res.end("HIIIIIIIIIIIIIII");
 });
 
-
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000...");
+});
