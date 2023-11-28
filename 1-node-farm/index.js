9c7edb92a5b126ab132dd9c88f17704f6a73b314
damiang <damiangildea12@gmail.com>
2023-11-28 09:28:19 +0000

    Creating simple api

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // fs.readFile("./starter/txt/start.txt", "utf-8", (err, data) => {
 //   fs.writeFile(`./starter/txt/output.txt`, `${data}`, "utf-8", err => {
@@ -7,10 +8,27 @@ const http = require("http");
 //   });
 // });
 
-const server = http.createServer((req, res) => {
-  res.end("HIIIIIIIIIIIIIII");
-});
+// const server = http.createServer((req, res) => {
+//   res.end("HIIIIIIIIIIIIIII");
+// });
 
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW!");
+  } else if (pathName === "/api") {
+    fs.readFile(
+      `${__dirname}/starter/dev-data/data.json`,
+      "utf-8",
+      (err, data) => {
+        const productData = JSON.parse(data);
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
+      }
+    );
+  }
+});
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening on port 8000...");
 });
