e66366d314f741ff70611684b9bc181d48fb09cb
damiang <damiangildea12@gmail.com>
2023-12-01 09:54:55 +0000

    Created async await version

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,29 +19,48 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
 
-    return writeFilePro("dog-img.txt", res.body.message);
-
-    // fs.writeFile("dog-img.txt", res.body.message, (err) => {
-    //   if (err) return console.log(err.message);
-    //   console.log("Random dog image saved to file");
-    // });
-  })
-  .then(() => {
+    await writeFilePro("dog-img.txt", res.body.message);
     console.log("Random dog image saved to file");
-  })
-  .catch((err) => {
-    console.log("NO FILE FOUND BUD");
-  });
+  } catch (err) {
+    console.log("ERRORRRRRR!!!!");
+  }
+};
 
-// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-//   console.log(`Breed: ${data}`);
-// });
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     return writeFilePro("dog-img.txt", res.body.message);
+
+//     // fs.writeFile("dog-img.txt", res.body.message, (err) => {
+//     //   if (err) return console.log(err.message);
+//     //   console.log("Random dog image saved to file");
+//     // });
+//   })
+//   .then(() => {
+//     console.log("Random dog image saved to file");
+//   })
+//   .catch((err) => {
+//     console.log("NO FILE FOUND BUD");
+//   });
+
+// // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+// //   console.log(`Breed: ${data}`);
+// // });
